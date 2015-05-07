cc44a740aac1453eb3bb2e8c5688fd97de38b2e5
chris lecompte & alex caste <student@epicodus.com>
2015-05-07 09:40:53 -0700

    adding post trains and post cities route

--- app.rb
+++ app.rb
@@ -18,6 +18,23 @@ get('/operator') do
   erb(:operator)
 end
 
+post('/trains') do
+  t_name = params.fetch("t_name")
+  train = Train.new({t_name: t_name, id: nil}).save()
+  @trains = Train.all()
+  @cities = City.all()
+  erb(:operator)
+end
+
+post('/cities') do
+  c_name = params.fetch("c_name")
+  city = City.new({c_name: c_name, id: nil}).save()
+  @cities = City.all()
+  @trains = Train.all()
+  erb(:operator)
+end
+
+
 get('/trains') do
   @trains = Train.all()
   erb(:trains)
